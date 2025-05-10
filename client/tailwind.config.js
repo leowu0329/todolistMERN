48dd3005d2ae83a7202451659bb7810c0c5b1248
leowu0329 <ryowuandjanet@gmail.com>
2025-05-10 15:30:58 +0800

    Adding Fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,10 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      fontFamily: {
+        normal: ['Regular'],
+        bold: ['Bold'],
+      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
