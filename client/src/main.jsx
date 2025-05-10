d1760f5e5ebef2d59239ee28bd128dba4b94d474
leowu0329 <ryowuandjanet@gmail.com>
2025-05-10 15:18:33 +0800

    React Router Dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+import Todos from './components/Todos.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/todos" element={<Todos />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
