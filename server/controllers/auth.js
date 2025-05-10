7383279cfbd2a78de52f38c3355e74682379e000
leowu0329 <ryowuandjanet@gmail.com>
2025-05-10 09:48:39 +0800

    Cookie based Login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -28,5 +28,36 @@ export async function register(reg, res, next) {
     data: newUser,
   });
 }
-export async function login(reg, res, next) {}
+export async function login(reg, res, next) {
+  const data = reg.body;
+  console.log(data);
+  if (!data.email || !data.password) {
+    return next(createError(400, 'Missing fields'));
+  }
+  await connectDB();
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(createError(404, 'Invalid credentials'));
+  }
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password,
+  );
+  if (!isPasswordValid) {
+    return next(createError(404, 'Invalid credentials'));
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT);
+  console.log(token);
+  res
+    .cookie('access_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    })
+    .status(200)
+    .json({
+      status: 'success',
+      message: 'User logged in Successfully',
+      data: user,
+    });
+}
 export async function logout(reg, res, next) {}
