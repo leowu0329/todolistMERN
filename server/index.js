4a1ba63c28771961e7dab469ae20ec93db30767e
leowu0329 <ryowuandjanet@gmail.com>
2025-05-10 09:29:08 +0800

    Error Handling

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({ error: message });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
