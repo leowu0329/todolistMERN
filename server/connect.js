92dd607b5d7cfeb4b4db911807afd15c47c2f4aa
leowu0329 <ryowuandjanet@gmail.com>
2025-05-10 09:22:20 +0800

    Connect with Mongodb

---
+++ server/connect.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connection = { isConnected: null };
+
+export const connectToDB = async () => {
+  try {
+    if (connection.isConnected) {
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log('Could not connect to database: ', error);
+  }
+};
